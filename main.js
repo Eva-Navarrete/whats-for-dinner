adccbf07f65ac3a14555de8f155cd23c947f35c6
Eva Navarrete <evanavarrete07@gmail.com>
2021-04-08 15:29:38 -0600

    Add data file and radio button selectors

---
+++ main.js
@@ -0,0 +1,4 @@
+var radioSide = document.querySelector('#side');
+var radioMainDish = document.querySelector('#mainDish');
+var radioDessert = document.querySelector('#dessert');
+var radioEntireMeal = document.querySelector('#entireMeal')
